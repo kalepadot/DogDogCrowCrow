495972c9e58c4081dd454f8b13177eaa52c9a083
Drew Anderson <drew@mohdi.com>
2020-06-07 23:33:07 -0700

    updates for landing page.

---
+++ src/game.js
@@ -0,0 +1,25 @@
+window.onload = function(){
+	// 1080 x 2160
+  var config = {
+    width: 800 ,
+    height:600,
+    backgroundColor: 0xFFFFFF,
+    scene: [TitleScene],
+    autoCenter: Phaser.Scale,
+
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false,
+        }
+    }
+  }
+
+  var game = new Phaser.Game(config);
+//   this.game.scale.pageAlignHorizontally = true;
+// this.game.scale.pageAlignVertically = true;
+// this.game.scale.refresh();
+
+
+
+}
